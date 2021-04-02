3b6c36e4be8abdb09fc70f1861ee26f457c4f876
Marla Schulz <marlaa.schulz@gmail.com>
2021-04-02 13:49:43 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,16 @@
+movie = "Ocean's Eleven"
+is_entertaining = true
+
+movie = "Harry Potter"
+
+
+num_4 = 2
+num_6 = 4
+
+product = num_4 * num_6
+
+
+favorite_foods = ["chocolate", "Pasta", "Ice Cream"]
+favorite_foods.shift
+
+puts movie.length
