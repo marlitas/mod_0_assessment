d5da5668388016a5276d9497c9ba5ff3130ee4fa
Marla Schulz <marlaa.schulz@gmail.com>
2021-04-02 14:03:17 -0600

    Complete developer hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => "Marla",
+  "favorite color" => "green",
+  "age" => 28,
+  "height ft" => 5.25,
+  "is hungry" => false,
+  "is tired" => true,
+  "dream vacations" => ["Greece", "Ireland", "Galapagos"],
+  "siblings" => ["Richard", "Kristina", "Claudia"],
+}
