ed7135200f46dcfd3efd2acadc77fea6bd896fed
Marla Schulz <marlaa.schulz@gmail.com>
2021-04-02 13:58:43 -0600

    Complete dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "type" => "t-rex",
+  "name" => "sally",
+  "height ft" => 10.5,
+  "weight lbs" => 5000,
+  "is carnivore" => true,
+  "is friendly" => false,
+  "favorite foods" => ["alligator", "raptor", "eggs"],
+  "top speeds mph" => [22, 24, 23, 25]
+}
